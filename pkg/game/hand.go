6e650aa981277b5548a5672cbd666cb4a8887cfb
antonio-muniz <ahnmhenrique@gmail.com>
2020-07-17 23:05:38 -0300

    Introduce hand data structure

---
+++ pkg/game/hand.go
@@ -0,0 +1,46 @@
+package game
+
+import (
+	"github.com/antonio-muniz/uno/pkg/card"
+)
+
+type Hand struct {
+	cards []card.Card
+}
+
+func NewHand() *Hand {
+	return &Hand{cards: make([]card.Card, 0, 7)}
+}
+
+func (h *Hand) AddCards(cards []card.Card) {
+	h.cards = append(h.cards, cards...)
+}
+
+func (h *Hand) Cards() []card.Card {
+	return h.cards
+}
+
+func (h *Hand) Empty() bool {
+	return len(h.cards) == 0
+}
+
+func (h *Hand) PlayableCards(lastPlayedCard card.Card) []card.Card {
+	var playableCards []card.Card
+	for _, candidateCard := range h.cards {
+		if Playable(candidateCard, lastPlayedCard) {
+			playableCards = append(playableCards, candidateCard)
+		}
+	}
+	return playableCards
+}
+
+func (h *Hand) RemoveCard(card card.Card) bool {
+	for index, cardInHand := range h.cards {
+		if cardInHand.Equal(card) {
+			h.cards[index] = h.cards[len(h.cards)-1]
+			h.cards = h.cards[:len(h.cards)-1]
+			return true
+		}
+	}
+	return false
+}
---
+++ pkg/game/hand_test.go
@@ -0,0 +1,70 @@
+package game_test
+
+import (
+	"testing"
+
+	"github.com/antonio-muniz/uno/pkg/card"
+	"github.com/antonio-muniz/uno/pkg/card/color"
+	"github.com/antonio-muniz/uno/pkg/game"
+	"github.com/stretchr/testify/require"
+)
+
+func TestAddCards(t *testing.T) {
+	hand := game.NewHand()
+	hand.AddCards([]card.Card{
+		card.NewNumberCard(color.Blue, 7),
+		card.NewWildCard(),
+	})
+	require.ElementsMatch(t, []card.Card{
+		card.NewNumberCard(color.Blue, 7),
+		card.NewWildCard(),
+	}, hand.Cards())
+}
+
+func TestEmpty(t *testing.T) {
+	hand := game.NewHand()
+	require.True(t, hand.Empty())
+	hand.AddCards([]card.Card{
+		card.NewNumberCard(color.Blue, 7),
+		card.NewWildCard(),
+	})
+	require.False(t, hand.Empty())
+}
+
+func TestPlayableCards(t *testing.T) {
+	hand := game.NewHand()
+	hand.AddCards([]card.Card{
+		card.NewNumberCard(color.Blue, 5),
+		card.NewNumberCard(color.Green, 8),
+		card.NewNumberCard(color.Green, 7),
+		card.NewWildCard(),
+		card.NewReverseCard(color.Yellow),
+		card.NewDrawTwoCard(color.Blue),
+	})
+	lastPlayedCard := card.NewNumberCard(color.Blue, 7)
+	playableCards := hand.PlayableCards(lastPlayedCard)
+	require.ElementsMatch(t, []card.Card{
+		card.NewNumberCard(color.Blue, 5),
+		card.NewNumberCard(color.Green, 7),
+		card.NewWildCard(),
+		card.NewDrawTwoCard(color.Blue),
+	}, playableCards)
+}
+
+func TestRemoveCard(t *testing.T) {
+	hand := game.NewHand()
+	hand.AddCards([]card.Card{
+		card.NewNumberCard(color.Blue, 5),
+		card.NewNumberCard(color.Green, 8),
+		card.NewNumberCard(color.Green, 7),
+		card.NewWildCard(),
+		card.NewReverseCard(color.Yellow),
+		card.NewDrawTwoCard(color.Blue),
+	})
+
+	result := hand.RemoveCard(card.NewReverseCard(color.Yellow))
+	require.True(t, result)
+
+	result = hand.RemoveCard(card.NewDrawTwoCard(color.Red))
+	require.False(t, result)
+}
